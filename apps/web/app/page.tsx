2840c73a70922a66656a9630fffe748acc395c90
Ahmet Kilinc <akx9@icloud.com>
2025-06-06 21:45:46 +0100

    lets just get this up

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
-import { Button } from "@workspace/ui/components/button"
-
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
+        <h1 className="text-4xl font-bold">oss now</h1>
+        <p>a place to share your open source projects and find new ones. coming soon.</p>
       </div>
     </div>
-  )
+  );
 }
