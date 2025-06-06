7bd6db1ae4a64cc41e2ff5cb202ce2373a45900b
Ahmet Kilinc <akx9@icloud.com>
2025-06-06 22:03:44 +0100

    woops

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col items-center justify-center gap-4 text-center px-4">
         <h1 className="text-4xl font-bold">oss now</h1>
         <p>a place to share your open source projects and find new ones. coming soon.</p>
       </div>
