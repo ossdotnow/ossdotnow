1c74480a53b1160706852c7ffcafea158f1d1c2f
Vicente Sanchez <Vikom.sanchez@gmail.com>
2025-06-07 20:46:36 -0300

    fix: make button as child (#3)

--- apps/web/components/layout/site-header.tsx
+++ apps/web/components/layout/site-header.tsx
@@ -11,12 +11,12 @@ const SiteHeader = () => {
           <span className="text-2xl font-medium">oss.now</span>
         </span>
         <div className="flex items-center gap-2">
-          <Button variant="ghost" size="icon" className="rounded-none">
+          <Button variant="ghost" size="icon" className="rounded-none" asChild>
             <Link href="https://l.oss.now/gh/" target="_blank" rel="noopener noreferrer">
               <Icons.github className="size-5 fill-white" />
             </Link>
           </Button>
-          <Button variant="ghost" size="icon" className="rounded-none">
+          <Button variant="ghost" size="icon" className="rounded-none" asChild>
             <Link href="https://l.oss.now/x/" target="_blank" rel="noopener noreferrer">
               <Icons.twitter className="size-5 fill-white" />
             </Link>
