62b31c69c5db7e34f660d6772cc679069c761f21
Ahmet Kilinc <akx9@icloud.com>
2025-06-09 17:30:41 +0100

    fix: removed padding from body (#13)

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -27,10 +27,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${fontSans.variable} ${fontMono.variable} overscroll-none bg-[#101010] px-2 font-sans antialiased`}
+        className={`${fontSans.variable} ${fontMono.variable} overscroll-none bg-[#101010] font-sans antialiased`}
       >
         <Providers>
-          {children}
+          <main className="px-2">{children}</main>
           <Analytics />
         </Providers>
       </body>
