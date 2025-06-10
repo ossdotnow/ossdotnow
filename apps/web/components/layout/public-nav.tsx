0812dcea66381d11b5123ef8ca24301cf5a5ee2e
Ahmet Kilinc <akx9@icloud.com>
2025-06-10 21:52:30 +0100

    add link to public nav

--- apps/web/components/layout/public-nav.tsx
+++ apps/web/components/layout/public-nav.tsx
@@ -2,12 +2,15 @@ import Link from '@/components/link';
 
 export default function PublicNav() {
   return (
-    <div className="flex items-center gap-2 text-sm">
+    <div className="mr-2 flex items-center gap-2 text-sm">
+      <Link href="/roadmap" event="roadmap_nav_click" className="p-2">
+        roadmap
+      </Link>
       <Link href="/projects" event="projects_nav_click" className="p-2">
-        Projects
+        projects
       </Link>
       <Link href="/about" event="about_nav_click" className="p-2">
-        About
+        about
       </Link>
     </div>
   );
