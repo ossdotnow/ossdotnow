f2abbea3c5ff19f348880f7de0d36c0aa4de24b2
Ahmet Kilinc <akx9@icloud.com>
2025-06-21 20:23:04 +0100

    fix

--- apps/web/app/(admin)/admin/layout.tsx
+++ apps/web/app/(admin)/admin/layout.tsx
@@ -8,9 +8,14 @@ import { SidebarInset, SidebarProvider } from '@workspace/ui/components/sidebar'
 import { AppSidebar } from '@workspace/ui/components/app-sidebar';
 import { notFound, redirect } from 'next/navigation';
 import { auth } from '@workspace/auth/server';
+import { env } from '@workspace/env/server';
 import { headers } from 'next/headers';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
+  if (env.NODE_ENV === 'production') {
+    notFound();
+  }
+
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -31,7 +36,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
     <SidebarProvider>
       <AppSidebar user={user} />
       <SidebarInset>
-        <header className="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear">
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
             <Breadcrumb>
               <BreadcrumbList>
