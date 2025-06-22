d1a098bb3adc0c2aff43866754852915e22dae96
Ahmet Kilinc <akx9@icloud.com>
2025-06-22 16:35:47 +0100

    remove meta

--- apps/web/app/(public)/layout.tsx
+++ apps/web/app/(public)/layout.tsx
@@ -1,11 +1,5 @@
 import { PoweredByNeon } from '@/components/layout/powered-by-neon';
 import { SiteHeader } from '@/components/layout/site-header';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'oss.now | Coming soon',
-  description: 'A place to share your open source projects and find new ones. Coming soon.',
-};
 
 export default function RootLayout({
   children,
