2cbcb62b4bbfab3421db2a2409dd81c74f17724d
Ahmet Kilinc <akx9@icloud.com>
2025-06-26 01:47:35 +0100

    some fixes

--- packages/api/src/routers/user.ts
+++ packages/api/src/routers/user.ts
@@ -1,4 +1,7 @@
 import { createTRPCRouter, protectedProcedure } from '../trpc';
+import { user } from '@workspace/db/schema';
+import { eq } from 'drizzle-orm';
+import z from 'zod';
 
 export const userRouter = createTRPCRouter({
   me: protectedProcedure.query(({ ctx }) => {
@@ -10,5 +13,18 @@ export const userRouter = createTRPCRouter({
         userAgent: ctx.session.userAgent,
       },
     };
+  }),
+  get: protectedProcedure.input(z.string()).query(({ ctx, input }) => {
+    if (input === 'me') {
+      return {
+        id: ctx.user.id,
+        name: ctx.user.name,
+        image: ctx.user.image,
+      };
+    }
+    const userData = ctx.db.query.user.findFirst({
+      where: eq(user.id, input),
+    });
+    return userData;
   }),
 });
