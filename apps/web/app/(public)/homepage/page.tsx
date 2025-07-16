39906a8ab07bf1d9e9f4ad6e7fb626864bed1fd1
Ayush Ranjan Sahoo <77970726+aysahoo@users.noreply.github.com>
2025-07-16 11:57:56 +0530

    search-comp

---
+++ apps/web/app/(public)/homepage/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Search from '@/components/search-comp'
+
+const homepage = () => {
+  return (
+    <div>
+        <Search />
+    </div>
+  )
+}
+
+export default homepage
