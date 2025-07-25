2d764346591d4ce3f8cc10f96dbdb47a6bd40d71
Ahmet Kilinc <akx9@icloud.com>
2025-07-25 15:18:13 +0100

    sentry fix?

--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -2,11 +2,11 @@
 // The added config here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import { env } from '@workspace/env/server';
+import { env } from '@workspace/env/client';
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: env.SENTRY_DSN,
+  dsn: env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Add optional integrations for additional features
   integrations: [Sentry.replayIntegration()],
