85e74f5a3e86941b5c5b7a38a93c7bb03d3cfe1d
Ahmet Kilinc <akx9@icloud.com>
2025-07-30 00:51:33 +0100

    fixes

--- packages/env/src/client.ts
+++ packages/env/src/client.ts
@@ -7,15 +7,12 @@ export const env = createEnv({
     NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL: z.string().optional(),
     // Sentry
     NEXT_PUBLIC_SENTRY_DSN: z.string().min(1),
-    // Databuddy
-    NEXT_PUBLIC_DATABUDDY_CLIENT_ID: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
     NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL:
       process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL,
     NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    NEXT_PUBLIC_DATABUDDY_CLIENT_ID: process.env.NEXT_PUBLIC_DATABUDDY_CLIENT_ID,
   },
   skipValidation: process.env.VERCEL_ENV !== 'production',
 });
